ddb46bf55ebc49b08fc643e0a98e4d5ca4793053
shehbaz-alphai <shehbaz@alphai.dev>
2023-10-08 14:43:28 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
-import express from 'express'
-const app=express()
-app.listen(3000,()=>{
-console.log("Server is runing on port 3000!");
+import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect()
+const app = express(process.env.MONGO).then(() => {
+    console.log('current to Monogodb!');
+}).catch((err) => {
+    console.log(err);
+})
+
+app.listen(3000, () => {
+    console.log("Server is runing on port 3000!");
 }
 );
