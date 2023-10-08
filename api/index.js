0c605f18534a5bd24509cde5d28dc8328ad371bc
shehbaz-alphai <shehbaz@alphai.dev>
2023-10-08 15:45:03 +0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
-mongoose.connect()
-const app = express(process.env.MONGO).then(() => {
-    console.log('current to Monogodb!');
-}).catch((err) => {
-    console.log(err);
-})
 
-app.listen(3000, () => {
-    console.log("Server is runing on port 3000!");
-}
-);
+const app = express();
+const PORT = 3000;
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB!');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+    app.use('/api/user', userRouter);
