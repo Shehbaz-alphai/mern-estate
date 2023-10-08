74d18e03c7328a2d066f666c6b6eed0584bd077b
shehbaz-alphai <shehbaz@alphai.dev>
2023-10-08 17:59:12 +0500

    create a middleware and a function to  handle possible errors

--- api/index.js
+++ api/index.js
@@ -4,12 +4,9 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 const PORT = 3000;
-
-
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB!');
@@ -20,6 +17,15 @@ mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology:
     .catch((err) => {
         console.error(err);
     });
-
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+// how to check middleware and function  to handle possible errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
